fbd9b01da66e8fbba77472c94a09603e9e2415d0
Herb Torres <herbtorres960@gmail.comm>
2024-02-19 21:11:21 -0500

    adds gh pages stuff

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  assetPrefix: "/grid-prezi-demo/",
+  basePath: "/grid-prezi-demo",
+  output: "export",
+  // images: {
+  //   domains: ["cdn.sanity.io"],
+  //   deviceSizes: [320, 640, 768, 1024, 1600],
+  //   loader: "custom",
+  // },
+};
 
 export default nextConfig;
